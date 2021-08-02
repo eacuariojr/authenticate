3b235055e7af505f416be69ea45e5d6b21e71b47
ErwinA <acuario.erwin00@gmail.com>
2021-08-02 14:36:05 -0700

    Simple out of the box Spring Boot Security setup

--- src/main/java/io/spamdev/authenticate/api/ContentViewer.java
+++ src/main/java/io/spamdev/authenticate/api/ContentViewer.java
@@ -1,5 +1,15 @@
 package io.spamdev.authenticate.api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ContentViewer
 {
+    @RequestMapping({"/hello"})
+    public String hello()
+    {
+        return "Hello World";
+    }
+
 }
