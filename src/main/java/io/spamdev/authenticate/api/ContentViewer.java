8bd16275089a24c9ed35be33167d85f87e61f255
ErwinA <acuario.erwin00@gmail.com>
2021-08-02 15:29:30 -0700

    fixed test api to actually display

--- src/main/java/io/spamdev/authenticate/api/ContentViewer.java
+++ src/main/java/io/spamdev/authenticate/api/ContentViewer.java
@@ -1,12 +1,15 @@
 package io.spamdev.authenticate.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ContentViewer
 {
-    @RequestMapping({"/hello"})
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello()
     {
         return "Hello World";
