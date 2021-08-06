203ac34be3ff878f6128ae990a85e4f1965eaa03
ErwinA <acuario.erwin00@gmail.com>
2021-08-06 12:09:02 -0700

    custom login page successfully logs in, but no jwt given

--- src/main/java/io/spamdev/authenticate/api/AuthenticateController.java
+++ src/main/java/io/spamdev/authenticate/api/AuthenticateController.java
@@ -1,5 +1,7 @@
 package io.spamdev.authenticate.api;
 
+import io.spamdev.authenticate.security.DbUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AuthenticateController
 {
+    @Autowired
+    DbUserDetailsService dbUserDetailsService;
+
     @GetMapping("/login")
     public String serveLogin()
     {
         return "/security/login";
     }
 
+    //Currently unused
+    //Overrides default spring security controller so user also gets jwt
     @PostMapping("/login")
     @ResponseBody
     public ResponseEntity<?> verifyLogin(@PathVariable("username") String username,
